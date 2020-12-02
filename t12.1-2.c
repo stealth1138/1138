e2b86f25c6af9e6cd42bbb27de18880531cd8b2f
stealth1138 <74520552+stealth1138@users.noreply.github.com>
2020-12-02 16:11:45 +0800

    使用for循环计算n的阶乘

---
+++ t12.1-2.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	int i = 2;
+	int ret = 1;
+	int n;
+	scanf("%d", &n);
+	for (i = 2; i <= n; i++)
+	{
+		ret *= i;
+	}
+	printf("%d\n", ret);
+	return 0;
+}

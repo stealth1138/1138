c444c001e156259e6f09ce5987b27d36d0fd7712
stealth1138 <74520552+stealth1138@users.noreply.github.com>
2020-12-04 18:25:14 +0800

    错误程序，传值于x，y并不能对a，b造成任何影响

---
+++ t12.4-1.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+void swap(int x, int y) 
+{
+	int tmp = x;
+	x = y;
+	y = tmp;
+}
+int main()
+{
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+	if (a > b)
+	{
+		swap(a, b);
+	}
+	if (b > c)
+	{
+		swap(b, c);
+	}
+	if (a > c)
+	{
+		swap(a, c);
+	}
+		printf("%d,%d,%d\n", a, b, c);
+	return 0;
+}
